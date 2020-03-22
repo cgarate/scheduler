7403976cf3d54913f526cff3343166cb91d18dfd
Carlos Garate <carlos.m.garate@gmail.com>
2020-03-22 15:11:01 -0400

    add hook to switch between visual modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 
 import "components/Appointment/styles.scss";
+import useVisualMode from "../../hooks/useVisualMode";
+import { EMPTY, SHOW, CREATE } from "../../constants";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 
-const Appointment = ({ time, interview }) => {
+const Appointment = ({ time, interview, interviewers }) => {
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+  const addAppointment = () => transition(CREATE);
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? (
+      {mode === EMPTY && <Empty onAdd={addAppointment} />}
+      {mode === SHOW && (
         <Show
           student={interview.student}
           interviewer={interview.interviewer.name}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={interviewers}
+          // onSave={}
+          onCancel={() => back()}
+        />
       )}
     </article>
   );
