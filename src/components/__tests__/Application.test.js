efd0e175380baf5b80d5a5e90faaccd562302400
Carlos Garate <carlos.m.garate@gmail.com>
2020-04-05 21:52:22 -0400

    add integration tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import {
   render,
@@ -24,6 +25,7 @@ describe("Application", () => {
     fireEvent.click(getByText("Tuesday"));
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     // NOTE: notice getByText is not destructured from the render in this case.
     // Get the root container
@@ -76,15 +78,16 @@ describe("Application", () => {
     ).toBeInTheDocument();
   });
 
-  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  it("loads data, deletes an interview and increases the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
     await waitForElement(() => getByText(container, "Archie Cohen"));
     const mondayDayListItems = getAllByTestId(container, "day")[0];
 
     // Get only the second article with appointment data
-    const appointmentToCancel = getAllByTestId(container, "appointment").find(
-    appointment => queryByText(appointment, "Archie Cohen")
-  );
+    const appointmentToCancel = getAllByTestId(
+      container,
+      "appointment",
+    ).find(appointment => queryByText(appointment, "Archie Cohen"));
 
     // Select the icon we need to click on to delete an appointment and click on it
     const cancelIcon = getByAltText(appointmentToCancel, "Delete");
@@ -102,14 +105,140 @@ describe("Application", () => {
     );
 
     // Check if the add button is back
-    expect(getByTestId(appointmentToCancel, "icon-add-appointment")).toBeInTheDocument();
+    expect(
+      getByTestId(appointmentToCancel, "icon-add-appointment"),
+    ).toBeInTheDocument();
 
     // Check the counter changed
     expect(
       getByText(mondayDayListItems, "2 spots remaining"),
     ).toBeInTheDocument();
-  })
-  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {})
-  it("shows the save error when failing to save an appointment", async () => {})
-  it("shows the delete error when failing to delete an existing appointment", async () => {})
+  });
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const mondayDayListItems = getAllByTestId(container, "day")[0];
+
+    // Get only the second article with appointment data
+    const appointmentToEdit = getAllByTestId(
+      container,
+      "appointment",
+    ).find(appointment => queryByText(appointment, "Archie Cohen"));
+
+    // Select the icon we need to click on to edit an appointment and click on it
+    const icon = getByAltText(appointmentToEdit, "Edit");
+    fireEvent.click(icon);
+
+    // Get the input text box
+    const inputStudentName = getByTestId(
+      appointmentToEdit,
+      "student-name-input",
+    );
+
+    // Update the input with the student's name
+    fireEvent.change(inputStudentName, { target: { value: "Guy Incognito" } });
+
+    // Get the Save button
+    const saveButton = getByText(appointmentToEdit, "Save");
+
+    // Click on the Save button
+    fireEvent.click(saveButton);
+
+    // Check that Status is on "Saving"
+    expect(getByText(appointmentToEdit, "Saving...")).toBeInTheDocument();
+    await waitForElementToBeRemoved(() =>
+      getByText(appointmentToEdit, "Saving..."),
+    );
+
+    expect(getByText(container, "Guy Incognito")).toBeInTheDocument();
+
+    // Check the counter is the same
+    expect(
+      getByText(mondayDayListItems, "1 spot remaining"),
+    ).toBeInTheDocument();
+  });
+
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const rootAppointmentArticle = getAllByTestId(container, "appointment")[0];
+
+    // Select the icon we need to click on to add an appointment and click on it
+    const addIcon = getByTestId(rootAppointmentArticle, "icon-add-appointment");
+    fireEvent.click(addIcon);
+
+    // Get the input text box
+    const inputStudentName = getByTestId(
+      rootAppointmentArticle,
+      "student-name-input",
+    );
+
+    // Update the input with the student's name
+    fireEvent.change(inputStudentName, { target: { value: "Guy Incognito" } });
+
+    // Get the Save button
+    const saveButton = getByText(rootAppointmentArticle, "Save");
+
+    // get one of the interviewer's avatars
+    const interviewerAvatar = getByAltText(
+      rootAppointmentArticle,
+      "Sylvia Palmer",
+    );
+
+    // Click on the avatar to select the interviewer
+    fireEvent.click(interviewerAvatar);
+
+    // Click on the Save button
+    fireEvent.click(saveButton);
+
+    // Check that Status is on "Saving"
+    expect(getByText(rootAppointmentArticle, "Saving...")).toBeInTheDocument();
+    await waitForElementToBeRemoved(() =>
+      getByText(rootAppointmentArticle, "Saving..."),
+    );
+
+    expect(
+      getByText(
+        rootAppointmentArticle,
+        "Oopsie Daysies! I could not save that appointment!",
+      ),
+    ).toBeInTheDocument();
+  });
+
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // Get only the second article with appointment data
+    const appointmentToCancel = getAllByTestId(
+      container,
+      "appointment",
+    ).find(appointment => queryByText(appointment, "Archie Cohen"));
+
+    // Select the icon we need to click on to delete an appointment and click on it
+    const cancelIcon = getByAltText(appointmentToCancel, "Delete");
+    fireEvent.click(cancelIcon);
+
+    // Get the comfirm button
+    const confirmButton = queryByText(appointmentToCancel, "Confirm");
+
+    // Click confirm
+    fireEvent.click(confirmButton);
+
+    // Check that Status is on "Deleting"
+    expect(getByText(appointmentToCancel, "Deleting...")).toBeInTheDocument();
+    await waitForElementToBeRemoved(() =>
+      queryByText(appointmentToCancel, "Deleting..."),
+    );
+
+    expect(
+      getByText(
+        appointmentToCancel,
+        "Oopsie Daysies! I could not delete the appointment!",
+      ),
+    ).toBeInTheDocument();
+  });
 });
